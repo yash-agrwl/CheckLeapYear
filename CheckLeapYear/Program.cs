b2c7bf1da46715e62d2fa590a86c7b1643903551
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-10 16:36:36 +0530

    added empty project file with welcome message

---
+++ CheckLeapYear/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CheckLeapYear
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Check Leap Year Problem\n");
+        }
+    }
+}
