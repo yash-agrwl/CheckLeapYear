97aceda88d6ee88ca942ce6844335b6919ed95a6
yash-agrwl <agarwalyash7012@gmail.com>
2022-03-10 17:40:34 +0530

    added uc1: Take Input & ensure its a 4-digit number.

--- CheckLeapYear/Program.cs
+++ CheckLeapYear/Program.cs
@@ -7,6 +7,21 @@ namespace CheckLeapYear
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Check Leap Year Problem\n");
+            int YearInput = TakeInput();
+            if (YearInput == -1)
+                Console.WriteLine("\nThe given Input is Invalid");
+            else
+                Console.WriteLine("\nThe give Input is Valid");
+        }
+
+        public static int TakeInput()
+        {
+            Console.Write("Enter Year: ");
+            string Input = Console.ReadLine();
+            if (Input.Length == 4)
+                return int.Parse(Input);
+            else
+                return -1;
         }
     }
 }
